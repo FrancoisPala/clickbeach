3b72707d6df300a579dfa3923e7e66ada185e4e5
FrancoisPala <francois.pala@epitech.eu>
2016-04-02 18:17:21 -0700

    removed map for array

--- object-game.js
+++ object-game.js
@@ -6,7 +6,8 @@
 /* Object Game */
 class Game {
     constructor (roomName, roomId) {
-        this.playerList = new Map(); /* map de player avec le score devant? */
+        this.playerList = []; /* map de player avec le score devant? Cannot be map (not enough entries) */
+        this.scoreBoard = [];
         this.playerCount = 1;
         this.roomName = roomName;
         this.roomId = roomId;
@@ -16,13 +17,40 @@ class Game {
     };
 
     addPlayerToList(player) {
-        this.playerList.set(player, 0);
+        console.log("--We here add a player to the list, playerId is: " + player.tempId);
+        this.playerList.push(player);
+        console.log("--The playerList is now composed of: " + this.playerList[0].tempId);
+        this.addPlayerToScoreboard(player);
+        console.log("--And now the scoreBoard is made of: " + this.scoreBoard[player.tempId])
         this.changePlayerCount(0);
+        //console.log("--Let's then access the player in the list, " + this.playerList[player].tempId)
     }
 
+    addPlayerToScoreboard(player){
+        console.log("--adding a player to the score, tempid = " + player.tempId);
+        this.scoreBoard[player.tempId] = 0;
+        console.log("--now added, scoreboard[tempid] = " + this.scoreBoard[player.tempId])
+    }
+
+    /*removePlayerFromScoreboard(player) {
+        let index = this.scoreBoard.indexOf(player.tempId);
+        if (index > -1) {
+            this.scoreBoard.splice(index, 1);
+        }
+        //this.scoreBoard.pop(player.tempId);
+    }*/
+
     removePlayerFromList(player) {
-        this.playerList.delete(player);
-        this.changePlayerCount(1);
+        this.removeFromArray(this.scoreBoard, player.tempId, 1); //remove the player from the scoreboard
+        this.removeFromArray(this.playerList, player, 1); //remove the player from the playerlist
+        this.changePlayerCount(1); //minus one on the player count
+    }
+
+    removeFromArray(array, element, number) {
+        let index = array.indexOf(element);
+        if (index > -1 && number > 0) {
+            array.splice(index, number);
+        }
     }
 
     incrementScore(player) {
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ function main() {
         console.log("client successfully connected");
         // Does the player have an account? We assume no for now but we should build the player object first
         let Player = require("./object-player.js")(socket, server.getGuestId); //gives us a player object
+        //let uniqueId = server.getGuestId; //already set above
         server.incrementGuestId();
         socket.emit("account info", omitPlayerSocket(Player)); //gotta remove the socket first
 
@@ -19,11 +20,15 @@ function main() {
 
         socket.on('createGame', function (jsonInfos) {
             let infos = JSON.parse(jsonInfos).split(" ");
-            console.log("client created game with infos 0 = " + infos[0] + " and infos 1 = " + infos[1]);
+            //console.log("client created game with infos Name = " + infos[0] + " and Client Id = " + infos[1]);
             server.incrementGameId();
             let gameRoom = require("./object-game.js")(infos[0], server.getGameId);
+            console.log("Game room created, player count = " + gameRoom.playerCount + " and roomName = " + gameRoom.roomName + " and roomId = " + gameRoom.roomId);
+            console.log("Now we add a player to the list, tempId = " + Player.tempId);
             gameRoom.addPlayerToList(Player);
+            console.log("So now the player is in the room, " + gameRoom.playerList[0]);
             server.addGameToList(gameRoom);
+            console.log("And now the room is in the list, " + server.gameList[0].roomId);
             joinGame(gameRoom);
         });
 
@@ -53,7 +58,7 @@ function main() {
             let gRTosend = server._.map(server.gameList.playerList, function(player) {
                 return server._.omit(player, "socket");
             });
-            server.io.emit("join game", gRTosend);
+            server.io.emit("join game", server._.omit(Player, "socket"));
         }
 
         function omitPlayerSocket(player) {
